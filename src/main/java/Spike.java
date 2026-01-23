7d932966647407610f60c310cb3c54c1a939807a
wy208 <wai.yan20822@gmail.com>
2026-01-24 01:39:24 +0800

    Rename Duke to Spike; Implement Greet and Exit; Update .gitignore file to include .class files

---
+++ src/main/java/Spike.java
@@ -0,0 +1,11 @@
+public class Spike {
+    public static void main(String[] args) {
+        String horizontalLine = "---------------------------------------------------";
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm Spike!");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
