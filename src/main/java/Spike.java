4f65359ab4a1211f5b7ed3c71577829c6c7407b4
wy208 <wai.yan20822@gmail.com>
2026-01-27 14:51:54 +0800

    Add echo function

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class Spike {
     public static void main(String[] args) {
-        String horizontalLine = "---------------------------------------------------";
-        System.out.println(horizontalLine);
+        // Greeting the user
+        String HORIZONTAL_LINE = "---------------------------------------------------";
+        System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm Spike!");
         System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+        System.out.println(HORIZONTAL_LINE);
+
+        Scanner in = new Scanner(System.in);
+        String line;
+
+        // Echo function
+        boolean isExit = false;
+        while (!isExit) {
+            line = in.nextLine();
+            System.out.println("  " + HORIZONTAL_LINE);
+
+            if (line.equals("bye")) {
+              System.out.println("  " + "Bye. Hope to see you again soon!");
+              isExit = true;
+            } else {
+                System.out.println("  " + line);
+            }
+            
+            System.out.println("  " + HORIZONTAL_LINE);
+        }
     }
 }
