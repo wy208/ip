94f12039af27b1acfc2ba028d9d897a3830a4df2
wy208 <wai.yan20822@gmail.com>
2026-01-29 21:46:30 +0800

    Implement mark and unmark functionality

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -13,27 +13,47 @@ public class Spike {
         String line;
 
         // Storing of tasks
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
-        // Echo function
         boolean isExit = false;
         while (!isExit) {
             line = in.nextLine().trim();
+            if (line.isEmpty()) {
+                continue;
+            }
 
             System.out.println("  " + HORIZONTAL_LINE);
 
+            // User exits
             if (line.equals("bye")) {
                 System.out.println("  " + "Bye. Hope to see you again soon!");
                 isExit = true;
+            // User lists tasks
             } else if (line.equals("list")) {
+                System.out.println("   Here are the tasks in your list: ");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println("   " + (i + 1) + ". " + tasks[i]);
                 }
+            // User marks task as done
+            } else if (line.startsWith("mark")) {
+                String[] parts = line.split(" ");
+                int index = Integer.parseInt(parts[1]) - 1;
+                tasks[index].markAsDone();
+                System.out.println("   Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[index]);
+            // User marks task as not done
+            } else if (line.startsWith("unmark")) {
+                String[] parts = line.split(" ");
+                int index = Integer.parseInt(parts[1]) - 1;
+                tasks[index].markAsNotDone();
+                System.out.println("   OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[index]);
+            // User adds new task
             } else {
-                tasks[taskCount] = line;
+                tasks[taskCount] = new Task(line);
                 taskCount++;
-                System.out.println("  added: " + line);
+                System.out.println("  Added: " + line);
             }
 
             System.out.println("  " + HORIZONTAL_LINE);
